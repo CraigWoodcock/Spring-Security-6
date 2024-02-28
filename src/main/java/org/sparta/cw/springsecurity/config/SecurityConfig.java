9384cc227031e95466ae8bca5a89a5c87e81427d
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-02-28 20:47:04 +0000

    optimised imports

--- src/main/java/org/sparta/cw/springsecurity/config/SecurityConfig.java
+++ src/main/java/org/sparta/cw/springsecurity/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package org.sparta.cw.springsecurity.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
