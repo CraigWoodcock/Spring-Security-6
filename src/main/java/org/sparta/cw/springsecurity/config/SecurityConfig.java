d16c9cb2e33453b2d6c6e7ef8f9f39d72f3efed6
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-02-29 09:41:42 +0000

    commenting out deny all and permit all methods to enforce security on secured endpoints

--- src/main/java/org/sparta/cw/springsecurity/config/SecurityConfig.java
+++ src/main/java/org/sparta/cw/springsecurity/config/SecurityConfig.java
@@ -11,15 +11,16 @@ public class SecurityConfig {
 
     // Custom security filter allows authenticated traffic to secured endpoints and allows all traffic to
     // unsecured endpoints.
-//    @Bean
-//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests((requests) -> requests
-//                .requestMatchers("/myAccount","/myCards", "/myLoans", "/myBalance").authenticated()
-//                .requestMatchers("/notices", "/contact", "/welcome").permitAll());
-//                http.formLogin(Customizer.withDefaults());
-//                http.httpBasic(Customizer.withDefaults());
-//        return http.build();
-//    }
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount","/myCards", "/myLoans", "/myBalance").authenticated()
+                .requestMatchers("/notices", "/contact", "/welcome").permitAll());
+                http.formLogin(Customizer.withDefaults());
+                http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+    
 
     // Deny all traffic to all endpoints
 //    @Bean
@@ -32,14 +33,14 @@ public class SecurityConfig {
 //    }
 
     // permits all traffic to all endpoints
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-                .anyRequest().permitAll());
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((requests) -> requests
+//                .anyRequest().permitAll());
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        return http.build();
+//    }
 
 
 }
